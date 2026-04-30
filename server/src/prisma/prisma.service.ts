4920cc5228d7edac50c8167077ff03b0cf62485a
Denis <denis_demidov_07@mail.ru>
2026-04-30 23:10:39 +0300

    add test logger

--- server/src/prisma/prisma.service.ts
+++ server/src/prisma/prisma.service.ts
@@ -1,13 +1,13 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
 import { PrismaPg } from '@prisma/adapter-pg';
 import { PrismaClient } from 'generated/prisma/client';
-
 @Injectable()
 export class PrismaService extends PrismaClient {
   constructor() {
     const connectionString = process.env.DATABASE_URL as string;
+    const logger = new Logger('test controller');
 
-    console.log(connectionString);
+    logger.error(connectionString);
 
     const adapter = new PrismaPg({
       connectionString,
