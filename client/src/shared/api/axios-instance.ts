2de5424e60626c3a0fe6f8b1610b20b7ba78fc5a
Denis <denis_demidov_07@mail.ru>
2026-06-02 20:23:09 +0300

    testing backend api

--- client/src/shared/api/axios-instance.ts
+++ client/src/shared/api/axios-instance.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 export const apiClient = axios.create({
-	baseURL: process.env.TEST_URL,
+	baseURL: process.env.BACK_URL,
 	withCredentials: true,
 	headers: {
 		'Content-Type': 'application/json',
